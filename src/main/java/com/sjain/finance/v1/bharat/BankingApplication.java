823ec8eda009a16c7f406acedb93d559502aa21e
Saksham Jain <jainsaksham67@gmail.com>
2024-04-19 19:25:11 +0530

    Initial commit

---
+++ src/main/java/com/sjain/finance/v1/bharat/BankingApplication.java
@@ -0,0 +1,13 @@
+package com.sjain.finance.v1.bharat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplication.class, args);
+	}
+
+}
